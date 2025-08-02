80ba9d858d52dedcd4182d050b27f6d373be9b98
Dhruv <dhruvrohilla9350@gmail.com>
2025-08-02 22:12:24 +0530

    add new sub

--- basic code/Backendtf.tf
+++ basic code/Backendtf.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "cc-tfstate-rgm"
-    storage_account_name = "cctfstatestg"   # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name = "cc-tfstate-rgm"
+#     storage_account_name = "cctfstatestg"   # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+#     container_name       = "tfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+#     key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+#   }
+# }
--- basic code/provider.tf
+++ basic code/provider.tf
@@ -10,5 +10,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "7e2be9af-a66a-4901-a4da-58be3e80552c"
+  subscription_id = "4a8937a7-b294-4a1f-8d31-79bbb1a4c17f"
 }
