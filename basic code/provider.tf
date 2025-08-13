c60042e6df375ff4d8a984c4768f8a74487faf69
Dhruv <dhruvrohilla9350@gmail.com>
2025-08-13 16:27:14 +0530

    add depends on

--- basic code/provider.tf
+++ basic code/provider.tf
@@ -12,3 +12,4 @@ provider "azurerm" {
   features {}
   subscription_id = "4a8937a7-b294-4a1f-8d31-79bbb1a4c17f"
 }
+
--- basic code/storageaccount.tf
+++ basic code/storageaccount.tf
@@ -1,4 +1,5 @@
 resource "azurerm_storage_account" "cc-storage" {
+  depends_on = [ azurerm_resource_group.cc-rg12 ]
   name                     = "ccstorage12kyo20250806"
   resource_group_name      = "cc-resource-group12"
   location                 = "eastus"
