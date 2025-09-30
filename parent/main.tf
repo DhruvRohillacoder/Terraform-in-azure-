7966f2de8b7671c2e31108c4bae3301ddb742db1
Dhruv <dhruvrohilla9350@gmail.com>
2025-09-30 13:05:34 +0530

    update

--- parent/main.tf
+++ parent/main.tf
@@ -5,18 +5,15 @@ module "STG" {
   source     = "../child/storage_account"
   depends_on = [module.rg]
 }
-
-
-
 module "vnet" {
   source     = "../child/vnet"
   depends_on = [module.rg]
 }
 
-# module "subnet" {
-#   source     = "../child/subnet"
-#   depends_on = [module.vnet]
-# }
+module "subnet" {
+  source     = "../child/subnet"
+  depends_on = [module.vnet]
+}
 
 # module "public_ip" {
 #   source     = "../child/Public_ip"
--- parent/provider.tf
+++ parent/provider.tf
@@ -1,23 +1,22 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.38.1"
+      source  = "hashicorp/azurerm" //specify the source of the provider here
+      version = "4.38.1"            //specify the version of the provider here
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "3c0d3e39-55e3-464c-b220-26aee31cecb9"
+  subscription_id = "3c0d3e39-55e3-464c-b220-26aee31cecb9" //enter your subscription id here
 }
+
 terraform {
   backend "azurerm" {
-    resource_group_name  = "apptf"
-    storage_account_name = "apptfstg" //this is the name of the storage account
-    container_name       = "tfstate"
+    resource_group_name  = "apptf"              //this is the name of the resource group
+    storage_account_name = "apptfstg"           //this is the name of the storage account
+    container_name       = "tfstate"            //this is the name of the container created inside the storage account
     key                  = "terraform1.tfstate" //this is the name of the state file
   }
 }
-
-
