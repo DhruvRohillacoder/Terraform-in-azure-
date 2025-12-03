657664417aa3f596a646823cb46bd748469c1ced
Dhruv <dhruvrohilla9350@gmail.com>
2025-12-03 13:57:27 +0530

    delet past code

--- child/resource_group/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_groups" {
-  type = map(object({
-    name     = string
-    location = string
-  }))
-  default = {
-    rg1 = { name = "dhruv1", location = "centralindia" }
-  }
-}
--- child/vnet/vnet.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_virtual_network" "this" {
-  for_each            = var.vnets
-  name                = each.key
-  address_space       = each.value.address_space
-  location            = each.value.location
-  resource_group_name = each.value.resource_group_name
-}
