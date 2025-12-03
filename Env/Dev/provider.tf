2ca101bac4f2e6857bc1e9a5414db4d8610a6e94
Dhruv <dhruvrohilla9350@gmail.com>
2025-12-03 19:48:27 +0530

    add BD

--- Env/Dev/provider.tf
+++ Env/Dev/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 3.0"
     }
   }
+  # terraform {
+  backend "azurerm" {
+    resource_group_name  = "dhruv-bd"
+    storage_account_name = "dhruvbdstg"
+    container_name       = "terrafornbd"
+    key                  = "dev.tfstate"
+    subscription_id      = "e18988e4-e1f4-4f06-9339-c63c8a22d3d9"
+  }
 }
 
 provider "azurerm" {
